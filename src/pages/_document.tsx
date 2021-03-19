2bc920ad7b8db45dc9bde493a7dbbfdb42e5c58a
patrikduch <duchpatrik@icloud.com>
2021-03-19 11:23:48 +0100

    React SSR: Styles flickering fix

---
+++ src/pages/_document.tsx
@@ -0,0 +1,46 @@
+import { ServerStyleSheet as StyledComponentSheets } from 'styled-components';
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="cs">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
+MyDocument.getInitialProps = async (ctx) => {
+  const styledComponentSheet = new StyledComponentSheets();
+  const materialUiSheets = new MaterialUiServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          styledComponentSheet.collectStyles(materialUiSheets.collect(<App {...props} />)),
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [
+        <React.Fragment key="styles">
+          {initialProps.styles}
+          {materialUiSheets.getStyleElement()}
+          {styledComponentSheet.getStyleElement()}
+        </React.Fragment>,
+      ],
+    };
+  } finally {
+    styledComponentSheet.seal();
+  }
+};
