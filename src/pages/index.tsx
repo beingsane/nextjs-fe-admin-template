29860fa3200df2366c7174d0adb1748bffc7dfcb
patrikduch <duchpatrik@icloud.com>
2021-03-27 21:38:00 +0100

    Redux: Deleted unnecessary reducer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { wrapper } from '@redux/index';
-import { checkServerSideCookie } from '@redux/actions/auth-actions';
 import MainContainer from '@components/layout/Main-Container';
 import { Grid, Typography } from '@material-ui/core';
 import { getProjectDetail } from '@redux/actions/project-detail-actions';
 import { NextPage } from 'next';
-
-console.log('MY_STEP', process.env.MY_STEP);
 
 /**
  * @function IndexPage Page component for root application path.
@@ -25,7 +22,6 @@ const IndexPage: NextPage = () => (
 
 export const getServerSideProps = wrapper.getServerSideProps(
   async (context) => {
-    checkServerSideCookie(context);
     await context.store.dispatch(getProjectDetail());
     return {
       props: {
--- src/redux/reducers/root-reducer.ts
+++ src/redux/reducers/root-reducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import authReducer from './auth-reducer';
 import projectDetailReducer from './project-detail-reducer';
 import userReducer from './user-reducer';
 
 const rootReducer = combineReducers({
-  authentication: authReducer,
   projectDetail: projectDetailReducer,
   users: userReducer
 });
