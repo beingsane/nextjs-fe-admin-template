767599effaf12725033b6e30086459a9881d6ce6
patrikduch <duchpatrik@icloud.com>
2021-03-27 22:03:53 +0100

    React.js - Header title component comments fix

--- src/components/skeleton/header/title/Header-Title.tsx
+++ src/components/skeleton/header/title/Header-Title.tsx
@@ -1,11 +1,17 @@
 import { connect } from 'react-redux';
 
+/**
+ * @interface IProps Component`s props interface.
+ */
 interface IProps {
     projectDetail?: {
         name: string;
     }
 }
 
+/**
+ * @function HeaderTitle Title component for logo title header.
+ */
 const HeaderTitle: React.FC<IProps> = ({ projectDetail }) => (
   <h1>{projectDetail.name}</h1>
 );
