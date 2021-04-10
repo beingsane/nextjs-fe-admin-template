852b19955879e94810dfcb562314e86b1a25665a
patrikduch <duchpatrik@icloud.com>
2021-04-10 10:17:30 +0200

    React.js - User login form basic validation

--- src/components/app/user-management/login/User-Login-Form.tsx
+++ src/components/app/user-management/login/User-Login-Form.tsx
@@ -1,28 +1,67 @@
 import React from 'react';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 import { Button, FormControl, Grid, Input, InputLabel } from '@material-ui/core';
 
 /**
  * @function UserLoginForm Form for login functionality.
- * @constructor
  */
 const UserLoginForm: React.FC = () => {
+  const initialValues =
+  {
+    emailField: '',
+    passwordField: ''
+  };
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema: yup.object().shape({
+      emailField: yup.string().required(),
+      passwordField: yup.string().required()
+    }),
+
+    onSubmit: async values => {
+      console.log(values);
+      debugger;
+    }
+  });
+
+  // console.log('Form values', formik.values);
+
+  console.log('Form errors', formik.errors);
+
   return (
-    <form>
+    <form onSubmit={ formik.handleSubmit }>
       <Grid item container>
         <Grid item xs={10}>
           <FormControl>
-            <InputLabel htmlFor="email-input">Email address</InputLabel>
-            <Input id="email-input" />
+            <InputLabel htmlFor="emailField">Email address</InputLabel>
+            <Input
+              type='text'
+              id="emailField"
+              onChange={ formik.handleChange }
+              value={ formik.values.emailField }
+              error={!!formik.errors.emailField}
+            />
+
+            {
+              formik.errors.emailField ? <div>{ formik.errors.emailField }</div> : null
+            }
+
           </FormControl>
         </Grid>
         <Grid item xs={10}>
           <FormControl>
             <InputLabel htmlFor="password-input">Password</InputLabel>
-            <Input id="password-input" />
+            <Input type='text' id="passwordField" onChange={ formik.handleChange } value={ formik.values.passwordField } error={!!formik.errors.passwordField} />
+
+            {
+              formik.errors.passwordField ? <div>{ formik.errors.passwordField }</div> : null
+            }
           </FormControl>
         </Grid>
         <Grid item xs={10}>
-          <Button>Log-in</Button>
+          <Button variant="contained" color="primary" type="submit">Log-in</Button>
         </Grid>
       </Grid>
     </form>
